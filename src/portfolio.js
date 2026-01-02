1e531a7c46da9b0f9fb4e1f867d80735f9d12749
falcomist1104 <rishi_malyala@outlook.com>
2026-01-02 19:25:47 +1100

    a

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,9 +21,9 @@ const illustration = {
 
 const greeting = {
   username: "Soumya Bogarapu",
-  title: "Hi all, Soumya",
+  title: "Hi all, I'm Soumya",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A global transformation leader."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
