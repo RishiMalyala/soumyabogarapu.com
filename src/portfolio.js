7a854ad04417f7b02421cc122a18bf47f7769a56
falcomist1104 <rishi_malyala@outlook.com>
2026-01-03 16:39:25 +1100

    aa

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Soumya Bogarapu",
   title: "Hi all, I'm Soumya",
   subTitle: emoji(
-    "A global transformation leader in Networking and datacentres."
+    "Global Network Transformation Leader (20+ Years) | SD-WAN & Data Center Architecture | Telecom Infrastructure Resilience & Secure Hybrid Network Design Architect."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
