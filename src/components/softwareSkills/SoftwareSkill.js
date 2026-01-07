0cff6e296572ac03f52771ec89448b84bb5e08da
falcomist1104 <rishi_malyala@outlook.com>
2026-01-07 22:33:54 +1100

    Update skills section

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,24 +1,34 @@
 import React from "react";
 import "./SoftwareSkill.scss";
-import {skillsSection} from "../../portfolio";
+import { skillsSection } from "../../portfolio";
 
 export default function SoftwareSkill() {
   return (
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
-            return (
-              <li
-                key={i}
-                className="software-skill-inline"
-                name={skills.skillName}
-              >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
-              </li>
-            );
-          })}
+          {skillsSection.softwareSkills.map((skill, i) => (
+            <li
+              key={i}
+              className="software-skill-inline"
+              name={skill.skillName}
+            >
+              {/* IMAGE-BASED ICON (preferred) */}
+              {skill.icon ? (
+                <img
+                  src={skill.icon}
+                  alt={skill.skillName}
+                  title={skill.skillName}
+                  className="skill-image-icon"
+                />
+              ) : (
+                /* FALLBACK: FontAwesome (if ever used) */
+                <i className={skill.fontAwesomeClassname}></i>
+              )}
+
+              <p>{skill.skillName}</p>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
