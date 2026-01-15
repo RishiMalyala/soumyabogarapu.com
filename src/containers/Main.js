5c945ed17ff77c4690b961b8175723990ecde218
falcomist1104 <rishi_malyala@outlook.com>
2026-01-15 15:30:34 +1100

    a

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -7,7 +7,7 @@ import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
-import Family from "../family/Family";
+import Family from "./family/Family";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
